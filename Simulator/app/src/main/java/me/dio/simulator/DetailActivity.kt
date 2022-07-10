f2f0de7a28815360d62975f33548bc5738523607
iedsonJc <iedsonbmxxtreme@gmail.com>
2022-07-09 23:54:27 -0300

    add image, id, textView

---
+++ Simulator/app/src/main/java/me/dio/simulator/DetailActivity.kt
@@ -0,0 +1,17 @@
+package me.dio.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.simulator.databinding.ActivityMainBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
