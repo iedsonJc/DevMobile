788ce50004c3a0fecc8d125df43c79a4684a6d1a
iedsonJc <iedsonbmxxtreme@gmail.com>
2022-07-12 11:15:12 -0300

    Desenvolvendo  dos Layout de listagens detalhes de partidas

--- Simulator/app/src/main/java/me/dio/simulator/DetailActivity.kt
+++ Simulator/app/src/main/java/me/dio/simulator/DetailActivity.kt
@@ -2,16 +2,19 @@ package me.dio.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import me.dio.simulator.databinding.ActivityMainBinding
+import me.dio.simulator.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
